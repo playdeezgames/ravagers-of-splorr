0c52db559d0af93bd82e14fe8a468850e28445a6
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-10-24 05:36:41 -0500

    before switch to avatar model

--- src/ROS.Model/Avatar/AvatarModel.cs
+++ src/ROS.Model/Avatar/AvatarModel.cs
@@ -1,13 +1,36 @@
-﻿using ROS.Persistence.World;
+﻿using ROS.Model.Enums;
+using ROS.Model.Extensions;
+using ROS.Persistence.Enums;
+using ROS.Persistence.World;
 
 namespace ROS.Model.Avatar
 {
-    internal class AvatarModel: IAvatarModel
+    internal class AvatarModel : IAvatarModel
     {
         private readonly IWorld _world;
         internal AvatarModel(IWorld world)
         {
             _world = world;
+        }
+
+        public int X => _world.X;
+
+        public int Y => _world.Y;
+
+        public Facings Facing => _world.Facing;
+
+        public void Move(Moves move)
+        {
+            var moveFacing = _world.Facing.GetMoveFacing(move);
+            int nextX = _world.Facing.GetNextX(_world.X, _world.Y);
+            int nextY = _world.Facing.GetNextY(_world.X, _world.Y);
+            _world.X = nextX;
+            _world.Y = nextY;
+        }
+
+        public void Turn(Turns turn)
+        {
+            _world.Facing = _world.Facing.MakeTurn(turn);
         }
     }
 }
--- src/ROS.Model/Avatar/IAvatarModel.cs
+++ src/ROS.Model/Avatar/IAvatarModel.cs
@@ -1,6 +1,14 @@
-﻿namespace ROS.Model.Avatar
+﻿using ROS.Model.Enums;
+using ROS.Persistence.Enums;
+
+namespace ROS.Model.Avatar
 {
     public interface IAvatarModel
     {
+        int X { get; }
+        int Y { get; }
+        Facings Facing { get; }
+        void Turn(Turns turn);
+        void Move(Moves move);
     }
 }
